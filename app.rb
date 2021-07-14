fe19cde647007a913893c528f21b29e8117ddd1a
lushcodes <lushcodes@gmail.com>
2021-07-14 09:45:06 +0100

    Set up view, controller and enviroment

--- app.rb
+++ app.rb
@@ -10,6 +10,12 @@ class Battle < Sinatra::Base
     'Testing infrastructure working!'
   end
 
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb(:play)
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -4,3 +4,13 @@ feature 'Testing infrastructure' do
     expect(page).to have_content 'Testing infrastructure working!'
   end
 end
+
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Spiderman'
+    fill_in :player_1_name, with: 'Batman'
+    click_button 'Submit'
+    expect(page).to have_content 'Spiderman vs. Batman'
+  end
+end
