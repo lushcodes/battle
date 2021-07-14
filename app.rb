40495374fabaff5df6ea3f347db75d9dfb28a4f6
lushcodes <lushcodes@gmail.com>
2021-07-14 15:40:16 +0100

    Added post route redirect

--- app.rb
+++ app.rb
@@ -2,18 +2,25 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Battle < Sinatra::Base
+  enable :sessions
   configure :development do
     register Sinatra::Reloader
   end
 
   get '/' do
-  erb(:index)
+    erb(:index)
   end
+
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:@player_1_name] = params[:player_1_name]
+    session[:@player_2_name]= params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
     erb(:play)
   end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
