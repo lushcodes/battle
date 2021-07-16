a1a63192cbe1e57b1f65dbbfe6b07eeab8aa4a8f
lushcodes <lushcodes@gmail.com>
2021-07-16 17:57:25 +0100

    added tests and functionality to reduce player 1 hp and allow them to be attacked

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -2,6 +2,14 @@ feature 'Attack' do
   scenario 'Attacks player with confirmation' do
     sign_in_and_play
     click_button 'Attack'
+    expect(page).to have_content 'Batman attacked Spiderman'
+  end
+
+  scenario 'be attacked by Player 2' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'OK'
+    click_button 'Attack'
     expect(page).to have_content 'Spiderman attacked Batman'
   end
 
@@ -12,4 +20,14 @@ feature 'Attack' do
     expect(page).not_to have_content 'Batman : 100'
     expect(page).to have_content 'Batman : 90'
   end
+
+  scenario 'reduce Player 1 HP by 10' do
+  sign_in_and_play
+  click_button 'Attack'
+  click_button 'OK'
+  click_button 'Attack'
+  click_button 'OK'
+  expect(page).not_to have_content 'Batman : 100'
+  expect(page).to have_content 'Spiderman : 90'
+end
 end
--- spec/features/check_hp_spec.rb
+++ spec/features/check_hp_spec.rb
@@ -1,6 +1,11 @@
 feature 'Check hp' do
   scenario 'Player 1 can see Player 2 hit points'do
   sign_in_and_play
+  expect(page).to have_content 'Spiderman : 100'
+  end
+
+  scenario 'Player 2 can see Player 1 hit points'do
+  sign_in_and_play
   expect(page).to have_content 'Batman : 100'
   end
 end
